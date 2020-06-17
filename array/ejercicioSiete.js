e2dc7c67cb1575192b510629aae1d24a2402e977
Never Adrian Sossa <nadris.xhhj@gmail.com>
2020-06-17 16:50:34 -0400

    Decimo commit

---
+++ array/ejercicioSiete.js
@@ -0,0 +1,8 @@
+const valores = [ 6, 0, 5, 2, 3, 8, 7, 4, 1, 9 ];
+const dobles = [];
+
+valores.forEach( element=>{
+    dobles.push(element*2);
+})
+console.log('simples:',valores);
+console.log('dobles: ',dobles);
