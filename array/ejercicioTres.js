e90ef6c0eb6e918aeb7aa4cb9cdbb54432fc0a0b
Never Adrian Sossa <nadris.xhhj@gmail.com>
2020-06-17 16:14:37 -0400

    Septimo commit

---
+++ array/ejercicioTres.js
@@ -0,0 +1,4 @@
+const playlist = ["Concrete and Gold", "The Line", "Sunday Rain", "Happy Ever After (Zero Hour)", "Arrows", "Dirty Water", "La Dee Da", "The Sky Is a Neighborhood", "Make It Right", "Run", "T-Shirt"];
+playlist.forEach(
+    (element,index)=>{ console.log(index,' - ', element )}
+)
